e87d6c74e471318c9a308b2a65903d570d551419
moosebot <austinvalle@gmail.com>
2018-02-09 23:25:49 -0500

    added basic print outs

--- cli.go
+++ cli.go
@@ -14,50 +14,45 @@ const (
 
 // CLI is the command line object
 type CLI struct {
-	// outStream and errStream are the stdout and stderr
-	// to write message from the CLI.
 	outStream, errStream io.Writer
 }
 
 // Run invokes the CLI with the given arguments.
 func (cli *CLI) Run(args []string) int {
 	var (
-		audio    bool
-		skipMeta bool
-		dir      string
+		audioFlag    bool
+		skipMetaFlag bool
+		dir          string
 
-		version bool
+		versionFlag bool
 	)
 
-	// Define option flag parse
 	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
 	flags.SetOutput(cli.errStream)
 
-	flags.BoolVar(&audio, "audio", false, "download audio mp3 only")
-	flags.BoolVar(&audio, "a", false, "download audio mp3 only(Short)")
-	flags.BoolVar(&skipMeta, "skip-meta", false, "skip the metadata edit step")
-	flags.BoolVar(&skipMeta, "s", false, "skip the metadata edit step(Short)")
-	flags.StringVar(&dir, "dir", "", "specify the download directory")
-	flags.StringVar(&dir, "d", "", "specify the download directory(Short)")
+	flags.BoolVar(&audioFlag, "a", false, "download audio mp3 only")
+	flags.BoolVar(&skipMetaFlag, "s", false, "skip the metadata edit step")
+	flags.StringVar(&dir, "d", "", "specify the download directory")
+	flags.BoolVar(&versionFlag, "v", false, "print version information")
 
-	flags.BoolVar(&version, "version", false, "Print version information and quit.")
-
-	// Parse commandline flag
 	if err := flags.Parse(args[1:]); err != nil {
 		return ExitCodeError
 	}
 
-	// Show version
-	if version {
-		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
+	if versionFlag {
+		fmt.Println(Version)
 		return ExitCodeOK
 	}
 
-	_ = audio
-
-	_ = skipMeta
-
-	_ = dir
+	if audioFlag {
+		fmt.Println("audio only detected")
+	}
+	if skipMetaFlag {
+		fmt.Println("skip metadata detected")
+	}
+	if dir != "" {
+		fmt.Printf("download directory: %v", dir)
+	}
 
 	return ExitCodeOK
 }
