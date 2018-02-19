3a6ed726f46eec8db051544ef8a99961c875efd9
Austin <austinvalle@gmail.com>
2018-02-19 00:55:58 -0500

    updated doc

--- internal/id3/id3.go
+++ internal/id3/id3.go
@@ -40,7 +40,7 @@ func CollectMetadataFromUser(videoInfo *ytdl.VideoInfo) *Metadata {
 	return &Metadata{Title: mp3Title, Artist: mp3Artist, Album: mp3Album}
 }
 
-// SetMetadata will
+// SetMetadata will apply metadata to the mp3 file's id3 tag
 func SetMetadata(mp3Location string, metadata *Metadata) {
 	tag, err := id3v2.Open(mp3Location, id3v2.Options{Parse: true})
 
